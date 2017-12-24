72f721557ad52249a603ba8bc0d2f9ff08e01f84
Xudong Zheng <7pkvm5aw@slicealias.com>
2017-12-24 09:36:13 -0500

    Sun, 24 Dec 2017 09:36:13 -0500

--- setup.sh
+++ setup.sh
@@ -10,6 +10,7 @@ fi
 
 ln -s "$base/.gitconfig" ~/.gitconfig
 ln -s "$base/.tmux.conf" ~/.tmux.conf
-ln -s "$base/.vimrc" ~/.vimrc
+
+echo "source $base/.vimrc" > ~/.vimrc
 
 cd .. && mkdir -p tmp/vim/{backup,swap,undo}
