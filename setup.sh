46b3414cef8caa1edac4648e61ff38f553330e56
Xudong Zheng <7pkvm5aw@slicealias.com>
2017-12-24 09:33:28 -0500

    Sun, 24 Dec 2017 09:33:28 -0500

--- setup.sh
+++ setup.sh
@@ -1,4 +1,15 @@
-base=$(realpath $(dirname $0))
-ln -s $base/.vimrc ~/.vimrc
-ln -s $base/.tmux.conf ~/.tmux.conf
-mkdir -p tmp/vim/{backup,swap,undo}
+cd $(dirname $0)
+base=$(pwd)
+
+if [[ $(uname) == "Darwin" ]]; then
+	echo "source \"$base/.bashrc\"" > ~/.bash_profile
+else
+	(echo "source \"$base/.bashrc\"" && cat ~/.bashrc) > tmp
+	mv tmp ~/.bashrc
+fi
+
+ln -s "$base/.gitconfig" ~/.gitconfig
+ln -s "$base/.tmux.conf" ~/.tmux.conf
+ln -s "$base/.vimrc" ~/.vimrc
+
+cd .. && mkdir -p tmp/vim/{backup,swap,undo}
