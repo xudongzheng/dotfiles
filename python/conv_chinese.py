1ce114c2a8db191bbfce49d38eb1db6aad0ea687
Xudong Zheng <7pkvm5aw@slicealias.com>
2025-02-23 00:22:38 -0500

    Sun, 23 Feb 2025 00:22:38 -0500

---
+++ python/conv_chinese.py
@@ -0,0 +1,25 @@
+import os
+import pathlib
+import sys
+
+# This scripts reads traditional Chinese on standard input and writes simplified
+# Chinese to standard output. The lookup file must be manually downloaded from
+# https://bit.ly/3QNdnDL.
+
+def main():
+	lookup_table = {}
+	conv_file = pathlib.Path(__file__).parent / "conv_chinese.txt"
+	with open(conv_file, "r") as file:
+		for line in file:
+			pieces = line.strip().split("\t")
+			if len(pieces) != 2:
+				raise Exception("failed to parse lookup table")
+			trad, simp = pieces
+			lookup_table[trad] = simp.split()[0]
+
+	input_text = sys.stdin.read()
+	output_text = "".join(lookup_table.get(char, char) for char in input_text)
+	print(output_text, end="")
+
+if __name__ == "__main__":
+	main()
